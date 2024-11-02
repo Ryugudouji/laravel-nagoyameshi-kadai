5417d34ff6482219bf16c06064fc8dcaa633b936
Ryugudouji <golden83caner@yahoo.co.jp>
2024-11-02 16:50:21 +0900

    中間テーブル作成・リレーションシップ設定

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Restaurant extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
 }
