953e819be3ce3b1ac14558dab56902c38c878a36
Ryugudouji <golden83caner@yahoo.co.jp>
2024-12-04 20:47:29 +0900

    会員側の会員管理機能を作る_再再提出

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -16,12 +17,13 @@ class UserSeeder extends Seeder
     {
         // User::factory()->count(100)->create();
 
-        User::firstOrcreate(
+        User::updateOrcreate(
             ['email' => 'user@example.com'], // ユーザーを特定する条件
             [
             'name' => '会員一郎',
             'kana' => 'カイインイチロウ',
             'email' => 'user@example.com',
+            'email_verified_at' => Carbon::now(),
             'password' => Hash::make('user1'),
             'postal_code' => '1234567',
             'address' => '愛知県',
