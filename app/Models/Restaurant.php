6529281d65cf44fff0a7617400709cecb9b1fd4a
Ryugudouji <golden83caner@yahoo.co.jp>
2024-12-14 16:38:06 +0900

    会員側レビュー機能Restaurantモデル修正

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -26,4 +26,10 @@ class Restaurant extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function ratingSortable($query, $direction)
+    {
+        return $query->withAvg('reviews', 'score')->orderBy('reviews_avg_score', $direction);
+    }
+
 }
